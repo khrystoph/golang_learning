e45ecce552265a0a64232fa6102f91dcd7f44c58
Bret Edwards <sliverman69@gmail.com>
2019-03-08 23:43:51 -0800

    fixed location of gosort.

--- gosort/gosort.go
+++ gosort/gosort.go
@@ -1,9 +1,12 @@
 //sort algorithms in go
 
-package gosort
+package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func mergesort(uintarray []int) (err error) {
@@ -11,31 +14,66 @@ func mergesort(uintarray []int) (err error) {
 	return nil
 }
 
-func quicksort(uintarray []int) (err error, quicksorted *[]int) {
+func quicksort(uintarray []int) (quicksorted *[]int, interr error) {
 	return nil, nil
 }
 
-func bubblesort(uintarray []int) (bubblesorted *[]int, err error) {
+func bubblesort(uintarray *[]int) (bubblesorted *[]int, err error) {
 	var (
-		tempA, i, j int
+		intlen   = len(*uintarray)
+		intarray = *uintarray
+		swapped  = true
 	)
-
-	for i = 0; i < len(uintarray)-1; i++ {
-		for j = 0; j < len(uintarray)-i-1; j++ {
-			if uintarray[j] > uintarray[j+1] {
-				tempA = uintarray[j]
-				uintarray[j] = uintarray[j+1]
-				uintarray[j+i] = tempA
+	for swapped {
+		swapped = false
+		for i := 0; i < (intlen - 1); i++ {
+			if intarray[i] > intarray[i+1] {
+				intarray[i], intarray[i+1] = intarray[i+1], intarray[i]
+				swapped = true
 			}
 		}
 	}
-	return nil, nil
+
+	bubblesorted = &intarray
+	err = nil
+
+	return bubblesorted, err
 }
 
-func arrayprinter(sortedarray []int) (err error) {
-	return
+func arrayprinter(slicepntr *[]int) (err error) {
+	var slicearray = *slicepntr
+	for i := range slicearray {
+		fmt.Printf("Value in slicepntr[%d] is %v\n", i, slicearray[i])
+	}
+	fmt.Printf("The address of pointer slicepointer is %p\n", slicepntr)
+	return nil
 }
 
 func main() {
+	var (
+		intarray    = []int{}
+		intarrayptr = &intarray
+		arraysize   = 10
+		val         int
+		err         error
+	)
+
+	flag.IntVar(&arraysize, "arraysize", 10, "enter an integer")
+
+	flag.Parse()
+
+	for i := 0; i < arraysize; i++ {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		val = r.Intn(1000)
+		intarray = append(intarray, val)
+	}
+
+	if intarrayptr, err = bubblesort(intarrayptr); err != nil {
+		fmt.Printf("Error sorting with bubble sort. Error msg: %v\n", err)
+	}
+
+	intarray = *intarrayptr
+	arrayprinter(&intarray)
+
 	return
 }

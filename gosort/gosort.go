78197a5df61211c8838407c3b76976b46f8a605a
J Bret Edwards <sliverman69@gmail.com>
2019-05-31 19:50:17 -0700

    finished threaded mergesort

--- gosort/gosort.go
+++ gosort/gosort.go
@@ -6,29 +6,41 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
+	"sync"
 	"time"
 )
 
 func init() {
 	flag.IntVar(&arraysize, "arraysize", 10, "enter an integer")
+	flag.Int64Var(&maxIntSize, "max", 1000, "enter an integer")
+	flag.BoolVar(&printArray, "p", false, "tells the program to print the arrays at the end, but before the times.")
 }
 
 var (
-	arraysize int
+	arraysize  int
+	maxIntSize int64
+	printArray bool
 )
 
 //NADA is used in mergesort as a sanity check to check if we should perform specific actions during sorting
-const NADA int = -1
+const NADA int64 = -1
+
+//THREADCOUNT is a constant to make adjusting the number of threads in waitgroup easier
+const THREADCOUNT = 5
+
+//MINLEN is the minimum length needed for goroutines to be effective. If we hit this value, we want to call regular mergesort
+//instead of another go routine so we can maximize our efficiency
+const MINLEN = 1000
 
 //DeepCopy is a helper function for mergesort to copy the slices created by recursion to a new slice
-func DeepCopy(vals []int) []int {
-	tmp := make([]int, len(vals))
+func DeepCopy(vals []int64) []int64 {
+	tmp := make([]int64, len(vals))
 	copy(tmp, vals)
 	return tmp
 }
 
 //mergesort recursively divides each array in half and sorts the smallest sizes, then merges them back after returning.
-func mergesort(uintarray []int) {
+func mergesort(uintarray []int64) {
 
 	if len(uintarray) > 1 {
 		mid := len(uintarray) / 2
@@ -69,18 +81,62 @@ func mergesort(uintarray []int) {
 
 //stub out threaded mergesort algorithms's function call.
 //See the readme for more info on tmergesort.
-func tmergesort(uintarray []int) (err error) {
-	fmt.Printf("entering threaded mergesort.")
-	return nil
+func tmergesort(uintarray []int64, r chan []int64) {
+	if len(uintarray) < MINLEN {
+		mergesort(uintarray)
+		r <- uintarray
+		return
+	}
+
+	leftchan := make(chan []int64)
+	rightchan := make(chan []int64)
+	middle := len(uintarray) / 2
+
+	go tmergesort(uintarray[:middle], leftchan)
+	go tmergesort(uintarray[middle:], rightchan)
+
+	luintarray := <-leftchan
+	ruintarray := <-rightchan
+
+	r <- merge(luintarray, ruintarray)
+	close(leftchan)
+	close(rightchan)
+	return
+
+}
+
+//merge is a function that actually performs the merge of two arrays for the mergesort algorithm.
+func merge(luintarray []int64, ruintarray []int64) (result []int64) {
+	result = make([]int64, len(ruintarray)+len(luintarray))
+	leftIndex, rightIndex := 0, 0
+
+	for i := 0; i < cap(result); i++ {
+		switch {
+		case leftIndex >= len(luintarray):
+			result[i] = ruintarray[rightIndex]
+			rightIndex++
+		case rightIndex >= len(ruintarray):
+			result[i] = luintarray[rightIndex]
+			leftIndex++
+		case luintarray[leftIndex] < ruintarray[rightIndex]:
+			result[i] = luintarray[leftIndex]
+			leftIndex++
+		default:
+			result[i] = ruintarray[rightIndex]
+			rightIndex++
+		}
+	}
+
+	return
 }
 
 //quicksort algorithm uses pivot value for divide and conquer to sort smaller arrays and put back together sorted
-func quicksort(uintarray []int) {
+func quicksort(uintarray []int64) {
 
 	if len(uintarray) > 1 {
 		pivotIndex := len(uintarray) / 2
-		var smallerItems = []int{}
-		var largerItems = []int{}
+		var smallerItems = []int64{}
+		var largerItems = []int64{}
 
 		for i := range uintarray {
 			val := uintarray[i]
@@ -96,24 +152,65 @@ func quicksort(uintarray []int) {
 		quicksort(smallerItems)
 		quicksort(largerItems)
 
-		var merged = append(append(append([]int{}, smallerItems...), []int{uintarray[pivotIndex]}...), largerItems...)
+		var merged = append(append(append([]int64{}, smallerItems...), []int64{uintarray[pivotIndex]}...), largerItems...)
 
 		for j := 0; j < len(uintarray); j++ {
 			uintarray[j] = merged[j]
 		}
 
 	}
-
+	return
 }
 
-//stubbing out heapsort algorithm's function call
-func heapsort(uintarray []int) (err error) {
-	fmt.Printf("entering heapsort")
-	return nil
+//heapify is a helper function for heapsort
+func heapify(items []int64, idx int64, size int64) {
+	l := 2*idx + 1 // left = 2*i + 1
+	r := 2*idx + 2 // right = 2*i + 2
+
+	var largest int64
+	if l <= size && items[l] > items[idx] {
+		largest = l
+	} else {
+		largest = idx
+	}
+
+	if r <= size && items[r] > items[largest] {
+		largest = r
+	}
+
+	if largest != idx {
+		t := items[idx]
+		items[idx] = items[largest]
+		items[largest] = t
+		heapify(items, largest, size)
+	}
+	return
+}
+
+//heapsort function creates a heap of unsorted items and
+func heapsort(items []int64) {
+	var L int64
+	L = int64(len(items)) //heap size
+	m := int64(L / 2)     //middle
+
+	for i := m; i >= 0; i-- {
+
+		heapify(items, i, L-1)
+	}
+
+	F := L - 1
+	for j := F; j >= 0; j-- {
+		t := items[0]
+		items[0] = items[j]
+		items[j] = t
+		F--
+		heapify(items, 0, F)
+	}
+	return
 }
 
 //Bubblesort. See Readme for more info.
-func bubblesort(uintarray []int) {
+func bubblesort(uintarray []int64) {
 	var (
 		intlen  = len(uintarray)
 		swapped = true
@@ -127,18 +224,18 @@ func bubblesort(uintarray []int) {
 			}
 		}
 	}
-
 	return
 }
 
-func builtInSort(uintarray []int) {
+//builtInSort is leveraging the package sort to do an efficient and stable sort
+func builtInSort(uintarray []int64) {
 	sort.Slice(uintarray, func(i, j int) bool {
 		return uintarray[i] < uintarray[j]
 	})
 	return
 }
 
-func arrayprinter(sliceint []int, arrayname string) (err error) {
+func arrayprinter(sliceint []int64, arrayname string) (err error) {
 	fmt.Printf("\nPrinting values for %s:\n", arrayname)
 	for i := range sliceint {
 		fmt.Printf("Value in %s[%d] is %v\n", arrayname, i, sliceint[i])
@@ -147,10 +244,10 @@ func arrayprinter(sliceint []int, arrayname string) (err error) {
 	return nil
 }
 
-func createArray() (intarray []int) {
+func createArray(arraysize int, maxSize int64) (intarray []int64) {
 	for i := 0; i < arraysize; i++ {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		val := r.Intn(1000)
+		val := r.Int63n(maxSize)
 		intarray = append(intarray, val)
 	}
 	return intarray
@@ -158,12 +255,15 @@ func createArray() (intarray []int) {
 
 func main() {
 	var (
-		intarray, bubbleint, mergeint, quickint, heapint, mergeintthread, builtInInt []int
+		bubbleint, mergeint, quickint, heapint, mergeintthread, builtInInt []int64
+		wg                                                                 sync.WaitGroup
 	)
+
+	messages := make(chan string, 5)
 
 	flag.Parse()
 
-	intarray = createArray()
+	intarray := createArray(arraysize, maxIntSize)
 
 	bubbleint = append(bubbleint, intarray...)
 	mergeint = append(mergeint, intarray...)
@@ -172,21 +272,56 @@ func main() {
 	mergeintthread = append(mergeintthread, intarray...)
 	builtInInt = append(builtInInt, intarray...)
 
-	bubblesort(bubbleint)
-	builtInSort(builtInInt)
-	quicksort(quickint)
-	mergesort(mergeint)
+	wg.Add(THREADCOUNT)
+	go func(somearray []int64) {
+		defer wg.Done()
+		bubblesort(somearray)
+		messages <- "Finished with Bubblesort."
+	}(bubbleint)
+	go func(somearray []int64) {
+		defer wg.Done()
+		builtInSort(somearray)
+		messages <- "Finished with Built-in Sort."
+	}(builtInInt)
+	go func(somearray []int64) {
+		defer wg.Done()
+		quicksort(somearray)
+		messages <- "Finished with QuickSort."
+	}(quickint)
+	go func(somearray []int64) {
+		defer wg.Done()
+		mergesort(somearray)
+		messages <- "Finished with MergeSort."
+	}(mergeint)
+	go func(somearray []int64) {
+		defer wg.Done()
+		heapsort(somearray)
+		messages <- "Finished with Heapsort."
+	}(heapint)
+	wg.Wait()
 
-	fmt.Printf("Pointers to arrays:\nintarray:%p\nbubbleint:%p\nmergeint:%p\n"+
-		"quickint:%p\nheapint:%p\nmergeintthread:%p", intarray, bubbleint, mergeint,
-		quickint, heapint, mergeintthread)
+	close(messages)
+	for message := range messages {
+		fmt.Println(message)
+	}
 
-	arrayprinter(intarray, "intarray")
-	arrayprinter(bubbleint, "bubbleint")
-	arrayprinter(builtInInt, "builtInInt")
-	arrayprinter(mergeint, "mergeint")
-	arrayprinter(quickint, "quickint")
-	arrayprinter(mergeintthread, "mergeintthread")
+	testmergestring := make(chan string)
+	rchan := make(chan []int64)
+	var testmergearray []int64
+	go func() {
+		tmergesort(mergeintthread, rchan)
+		testmergestring <- "Finished with merge."
+	}()
+	testmergearray = <-rchan
 
-	return
+	if printArray {
+		arrayprinter(intarray, "intarray")
+		arrayprinter(bubbleint, "bubbleint")
+		arrayprinter(builtInInt, "builtInInt")
+		arrayprinter(mergeint, "mergeint")
+		arrayprinter(quickint, "quickint")
+		arrayprinter(heapint, "heapint")
+		arrayprinter(testmergearray, "mergeintthread")
+	}
+	close(testmergestring)
 }
